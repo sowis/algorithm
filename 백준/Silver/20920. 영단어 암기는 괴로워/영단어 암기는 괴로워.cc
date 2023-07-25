451ada326a6ab2857aefb95b5a0134ed4e78f053
sowis <39920741+sowis@users.noreply.github.com>
2023-07-26 06:11:03 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 128 ms, Memory: 14772 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+typedef struct _data {
+	string str;
+	int len;
+	int appear;
+
+	bool operator< (const _data& d) const {
+		if (this->appear != d.appear) {
+			return this->appear > d.appear;
+		}
+
+		if (this->len != d.len) {
+			return this->len > d.len;
+		}
+
+		return this->str < d.str;
+	}
+} Data;
+
+int main(void) {
+	int N, M;
+	cin >> N >> M;
+
+	unordered_map<string, int> exists;
+	vector<Data> datas;
+	for (int i = 0; i < N; ++i) {
+		string str;
+		cin >> str;
+
+		if (str.size() < M) {
+			continue;
+		}
+
+		if (exists.find(str) != exists.end()) {
+			++datas[exists[str]].appear;
+		}
+		else {
+			exists[str] = (int)datas.size();
+			datas.push_back({ str, (int)str.size(), 1 });
+		}
+	}
+
+	sort(datas.begin(), datas.end());
+
+	for (const Data& data : datas) {
+		cout << data.str << "\n";
+	}
+
+	return 0;
+}
