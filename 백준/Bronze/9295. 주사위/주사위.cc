b76c534c46ff8a41a0b5182812df99a7c047e6e2
sowis <39920741+sowis@users.noreply.github.com>
2023-09-10 07:21:54 +0900

    [Bronze III] Title: 주사위, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(void) {
+	int T;
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; ++i) {
+		int a, b;
+		scanf("%d%d", &a, &b);
+
+		printf("Case %d: %d\n", i + 1, a + b);
+	}
+
+	return 0;
+}
