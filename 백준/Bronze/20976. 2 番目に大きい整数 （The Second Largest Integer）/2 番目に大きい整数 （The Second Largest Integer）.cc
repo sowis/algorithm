22468dba7203db0224b98a0c7da6d0123d28ddcd
sowis <39920741+sowis@users.noreply.github.com>
2023-10-08 13:19:38 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 0 ms, Memory: 1172 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.cc
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	vector<int> arr(3);
+	
+	for (int& num : arr) {
+		scanf("%d", &num);
+	}
+
+	sort(arr.begin(), arr.end());
+
+	printf("%d\n", arr[1]);
+
+	return 0;
+}
