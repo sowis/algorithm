42c8680042f53fb082359b4184fa7180184f7db1
sowis <39920741+sowis@users.noreply.github.com>
2024-01-04 13:08:38 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 128 ms, Memory: 23568 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -6,22 +6,24 @@
 using namespace std;
 
 int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
 	string s;
 	cin >> s;
 
 	int q;
 	cin >> q;
 
-	vector<vector<int>> psum('z' - 'a' + 1, vector<int>(s.size() + 1));
-	for (char c = 'a'; c <= 'z'; ++c) {
-		psum[c - 'a'][0] = ((s[0] == c) ? 1 : 0);
-		for (int i = 1; i < s.size(); ++i) {
-			psum[c - 'a'][i] = psum[c - 'a'][i - 1];
-
-			if (s[i] == c) {
-				++psum[c - 'a'][i];
-			}
+	vector<vector<int>> psum('z' - 'a' + 1, vector<int>(s.size() + 1, 0));
+	psum[s[0] - 'a'][0] = 1;
+	for (int i = 1; i < s.size(); ++i) {
+		for (int k = 0; k < 'z' - 'a' + 1; ++k) {
+			psum[k][i] = psum[k][i - 1];
 		}
+
+		++psum[s[i] - 'a'][i];
 	}
 
 	for (int i = 0; i < q; ++i) {
@@ -34,7 +36,7 @@ int main(void) {
 			result -= psum[c - 'a'][begin - 1];
 		}
 
-		printf("%d\n", result);
+		cout << result << "\n";
 	}
 
 	return 0;
