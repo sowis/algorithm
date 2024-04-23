af31c38f56874970b49a4eac702e865e7c32ba48
sowis <39920741+sowis@users.noreply.github.com>
2024-04-24 06:44:02 +0900

    [Platinum I] Title: 연속합과 쿼리, Time: 108 ms, Memory: 7876 KB -BaekjoonHub

---
+++ 백준/Platinum/16993. 연속합과 쿼리/연속합과 쿼리.cc
@@ -0,0 +1,111 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Segtree {
+	typedef struct _data {
+		bool valid;
+		int sum;
+		int left_sum;
+		int right_sum;
+		int total_sum;
+	} Data;
+
+private:
+	int N;
+	vector<int> sums;
+	vector<int> left_sums;
+	vector<int> right_sums;
+	vector<int> total_sums;
+
+	void init(const vector<int>& origin, const int begin, const int end, const int idx) {
+		if (end - begin == 1) {
+			this->sums[idx] = origin[begin];
+			this->left_sums[idx] = origin[begin];
+			this->right_sums[idx] = origin[begin];
+			this->total_sums[idx] = origin[begin];
+			return;
+		}
+
+		const int mid = (begin + end) / 2;
+		this->init(origin, begin, mid, idx * 2);
+		this->init(origin, mid, end, idx * 2 + 1);
+		
+		this->sums[idx] = this->sums[idx * 2] + this->sums[idx * 2 + 1];
+		this->left_sums[idx] = max(this->left_sums[idx * 2], this->sums[idx * 2] + this->left_sums[idx * 2 + 1]);
+		this->right_sums[idx] = max(this->right_sums[idx * 2] + this->sums[idx * 2 + 1], this->right_sums[idx * 2 + 1]);
+		this->total_sums[idx] = max(this->total_sums[idx * 2], this->total_sums[idx * 2 + 1]);
+		this->total_sums[idx] = max(this->total_sums[idx], this->right_sums[idx * 2] + this->left_sums[idx * 2 + 1]);
+	}
+
+	Data get_max_sum(const int target_begin, const int target_end, const int begin, const int end, const int idx) const {
+		if (target_end <= begin || end <= target_begin) {
+			return { false, 0, 0, 0, 0 };
+		}
+
+		if (target_begin <= begin && end <= target_end) {
+			return { true, this->sums[idx], this->left_sums[idx], this->right_sums[idx], this->total_sums[idx] };
+		}
+
+		const int mid = (begin + end) / 2;
+		const Data left_data = get_max_sum(target_begin, target_end, begin, mid, idx * 2);
+		const Data right_data = get_max_sum(target_begin, target_end, mid, end, idx * 2 + 1);
+
+		if (left_data.valid == false) {
+			return right_data;
+		}
+
+		if (right_data.valid == false) {
+			return left_data;
+		}
+
+		const int sum = left_data.sum + right_data.sum;
+		const int left_sum = max(left_data.left_sum, left_data.sum + right_data.left_sum);
+		const int right_sum = max(left_data.right_sum + right_data.sum, right_data.right_sum);
+		int total_sum = max(left_data.total_sum, right_data.total_sum);
+		total_sum = max(total_sum, left_data.right_sum + right_data.left_sum);
+		return { true, sum, left_sum, right_sum, total_sum };
+	}
+
+public:
+	Segtree(const vector<int>& origin)
+		:N((int)origin.size()) {
+		this->sums = vector<int>(this->N * 4, 0);
+		this->left_sums = vector<int>(this->N * 4, 0);
+		this->right_sums = vector<int>(this->N * 4, 0);
+		this->total_sums = vector<int>(this->N * 4, 0);
+
+		this->init(origin, 0, this->N, 1);
+	}
+
+	int get_max_sum(const int target_begin, const int target_end) const {
+		return this->get_max_sum(target_begin, target_end, 0, this->N, 1).total_sum;
+	}
+};
+
+int main(void) {
+	int N;
+	scanf("%d", &N);
+
+	vector<int> origin(N);
+	for (int& num : origin) {
+		scanf("%d", &num);
+	}
+
+	Segtree segtree(origin);
+
+	int M;
+	scanf("%d", &M);
+
+	for (int a = 0; a < M; ++a) {
+		int i, j;
+		scanf("%d%d", &i, &j);
+
+		const int result = segtree.get_max_sum(i - 1, j);
+		printf("%d\n", result);
+	}
+
+	return 0;
+}
