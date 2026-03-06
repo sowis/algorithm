b5be62c0fcc3b69be5dbadd36840aa8241c8f6bb
sowis <39920741+sowis@users.noreply.github.com>
2026-03-06 15:00:46 +0900

    [Silver III] Title: 2 타워, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10407. 2 타워/2 타워.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	string str;
+	cin >> str;
+
+	if (str == "1") {
+		cout << 2;
+	}
+	else {
+		cout << 1;
+	}
+
+	return 0;
+}
